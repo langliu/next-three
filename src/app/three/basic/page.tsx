f6b748d8ad5a238107d86b7a1df7bcb626286703
刘浪 <ll809721414@outlook.com>
2023-03-16 00:23:00 +0800

    feat: ✨ 新增基础box展示

---
+++ src/app/three/basic/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+import { Box } from './components/Box';
+
+export default function App() {
+  return (
+    <Canvas shadows dpr={[1, 2]}>
+      <ambientLight intensity={1} color='red' />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
